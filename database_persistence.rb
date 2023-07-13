9b0e8b1fe940a058c3389e2385b49ad88d321e40
Raul Fernandez <rafeye20@hotmail.com>
2023-07-13 07:50:00 -0500

    updated gemfile and dockerfile

--- database_persistence.rb
+++ database_persistence.rb
@@ -2,11 +2,12 @@ require "pg"
 
 class DatabasePersistence
   def initialize(logger)
-    @db = if Sinatra::Base.production?
-      PG.connect(ENV['DATABASE_URL'])
-    else
-      PG.connect(dbname: "todos")
-    end
+    # @db = if Sinatra::Base.production?
+    #   PG.connect(ENV['DATABASE_URL'])
+    # else
+    #   PG.connect(dbname: "todos")
+    # end
+    @db = PG.connect(ENV['DATABASE_URL'])
     @logger = logger
   end
 
